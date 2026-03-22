bbd77c8ef9db73aa7462e48565c03b25950bc922
imanisterling-svg <imanisterling@gmail.com>
2026-03-21 22:19:53 -0500

    new changes

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,16 +10,24 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+    //    private void OnCounterClicked(object? sender, EventArgs e)
+    //    {
+    //        count++;
+
+    //        if (count == 1)
+    //            CounterBtn.Text = $"Clicked {count} time";
+    //        else
+    //            CounterBtn.Text = $"Clicked {count} times";
+
+    //        SemanticScreenReader.Announce(CounterBtn.Text);
+    //    }
+    //}
+
+        private async Task OnEnterClicked(object? sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushAsync(new LayoutExample());
         }
     }
 
